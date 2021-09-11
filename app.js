fcf458972490b95d56711dde48d584f9bd145a84
gigu007 <gideonukaegbu007@gmail.com>
2021-09-11 06:48:17 +0100

    initial commit

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const express=require('express');
 const app=express();
 require('./startup/db')();
 require('./startup/routes')(app);
+require('./startup/prod')(app)
 const port=process.env.PORT ||3000;
 app.listen(port,()=>console.log(`listening on port ${port}`)); 
